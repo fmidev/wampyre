cc47242a91ec86f1bafcf979e363f276af10cff0
Tuomas Karna <tuomas.karna@gmail.com>
2018-11-08 15:36:30 +0200

    update bathymetry example

--- examples/bathymetry/bathymetry_example.py
+++ examples/bathymetry/bathymetry_example.py
@@ -13,8 +13,16 @@ plot_bathymetry(b, imgfile='bathymetry_HKI.png')
 # write out in WAM ASCII format
 b.write_ascii_wamtopo(outfile='wamtopo_new.asc')
 
+# write out in netCDF format
+b.write_netcdf(outfile='bathymetry.nc')
+
 # read bathymetry from WAM ASCII file
 b = Bathymetry.read_ascii_wamtopo('wamtopo_HKI.asc')
 
 # plot bathymetry, save as PNG image
 plot_bathymetry(b, imgfile='bathymetry_HKI_02.png')
+
+# read netCDF file
+b = Bathymetry.read_netcdf('bathy_meter_GOF_2017-04-25.nc')
+plot_bathymetry(b, imgfile='bathymetry_GOF.png')
+
